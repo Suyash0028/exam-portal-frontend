30ebc4fbed72de7f7ed46a915ca5a0fdc4d4ea3d
Suyash Kulkarni <suyashkulkarni43@gmail.com>
2024-06-25 21:41:22 -0400

    Updated app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { AuthProvider } from './Context/AuthContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import MainApp from './Components/MainApp';
+import Routes from '../Routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
+      <Routes />
       <ToastContainer />
-      <MainApp />
     </AuthProvider>
   );
 };
