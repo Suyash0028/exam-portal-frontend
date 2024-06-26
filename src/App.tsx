c865a4f6793670fdb4da1dab43367308fbec48a5
Suyash Kulkarni <suyashkulkarni43@gmail.com>
2024-06-25 21:48:49 -0400

    Removed routes file

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { AuthProvider } from './Context/AuthContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Routes from '../Routes';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import MainApp from './Components/MainApp';
+
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Routes />
+      <MainApp />
       <ToastContainer />
     </AuthProvider>
   );
